9ef0889c1405d1ce4175ac7e947e778c6548403d
Shabeeb P <shabeebp185@gmail.com>
2026-02-09 10:50:01 +0530

    Init

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import vercel from 'vite-plugin-vercel';
+
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  build: {
+    outDir: ".vercel/output/static",
+  },
   server: {
     host: "::",
     port: 8080,
@@ -12,7 +17,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react(), vercel(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
